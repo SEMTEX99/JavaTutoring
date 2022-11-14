a73e62235e1210b0397aa4a3f0a92d6c8b3730d7
SEMTEX99 <sinisamilosevic19@gmail.com>
2022-11-14 13:19:02 +0100

    done maybe

---
+++ Hello.java
@@ -0,0 +1,37 @@
+public class Hello {
+    public static void main(String[] args) {
+        String text = "ligmaballssonhagotembuteverytimethereisabugilooseanotherbitofsanityandtrytonotkillmyselfbecauseofthisballoonsproblemsinceitissomethingabeginnerwouldknowandidontknowshitsogodhelpmeifidontresorttokillingmyselffoverthisretardedproblemhahahahahahahahahahgodie";
+        int count = 0;
+        int b = 0;
+        int a = 0;
+        int l = 0;
+        int o = 0;
+        int n = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == 'b') {
+                b++;
+            }
+            if (text.charAt(i) == 'a') {
+                a++;
+            }
+            if (text.charAt(i) == 'l') {
+                l++;
+            }
+            if (text.charAt(i) == 'o') {
+                o++;
+            }
+            if (text.charAt(i) == 'n') {
+                n++;
+            }
+        }
+        while (b >= 1 && a >= 1 && l >= 2 && o >= 2 && n >= 1) {
+            count++;
+            b--;
+            a--;
+            l = l - 2;
+            o = o - 2;
+            n--;
+        }
+        System.out.println(count);
+    }
+}
