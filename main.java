ce13af28b15a16e46091cc84279cbb370d40db9a
SEMTEX99 <sinisamilosevic19@gmail.com>
2022-11-15 15:00:29 +0100

    output formatting

--- main.java
+++ main.java
@@ -1,14 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
         Hello solution = new Hello();
-        Scanner InputText = new Scanner(System.in);
-        System.out.println("Enter a string");
-        String inputtext = InputText.nextLine();
-        Scanner InputWord = new Scanner(System.in);
-        System.out.println("Enter a word");
-        String inputWord = InputWord.nextLine();
-        System.out.println("the word "+ inputWord + "can be found "+ solution.solution(inputtext, inputWord)+ " times in the string you inputed");
+        File file = new File("input.txt");
+        try (Scanner InputText = new Scanner(file)) {
+            String inputWord = InputText.nextLine();
+            int line = 1;
+            while (InputText.hasNextLine()) {
+                line++; 
+                String inputtext = InputText.nextLine();
+                System.out.println("the word " + inputWord + " can be found " + solution.solution(inputtext, inputWord) + " times in the string at line "+line);
+                try {
+                    FileWriter myWriter = new FileWriter("output.txt");
+                    myWriter.write("the word " + inputWord + " can be found " + solution.solution(inputtext, inputWord) + " times in the string at line "+line);
+                    myWriter.close();
+                    System.out.println("Successfully wrote to the file.");
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-    
 }
