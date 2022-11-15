a3fc5cb73d0a8b699e1cdf7aa996a9dc5f502815
SEMTEX99 <sinisamilosevic19@gmail.com>
2022-11-15 16:42:09 +0100

    Exception handling added

--- main.java
+++ main.java
@@ -3,22 +3,25 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 public class main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InvalidCharacterException {
         Hello solution = new Hello();
         File file = new File("input.txt");
-        try (Scanner InputText = new Scanner(file)) {
+        try (Scanner InputText = new Scanner(file); PrintWriter writer = new PrintWriter("output.txt")) {
             String inputWord = InputText.nextLine();
             int line = 1;
-            PrintWriter writer = new PrintWriter("output.txt");
             while (InputText.hasNextLine()) {
                 line++;
                 String inputtext = InputText.nextLine();
                 assert inputtext.length() > 0;
                 writer.println("the word " + inputWord + " can be found " + solution.solution(inputtext, inputWord) + " times in the string at line " + line);
+                if (inputtext.matches("[^a-zA-Z]+")) {
+                    throw new InvalidCharacterException("Only alphabet characters are accepted");
+                }
             }
-            writer.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
+        } catch (InvalidCharacterException e) {
+            System.out.println(e.getMessage());
         } catch (Exception e) {
             System.out.println("Oops Something went wrong :(");
         } finally {
